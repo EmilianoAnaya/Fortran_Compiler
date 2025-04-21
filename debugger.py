5b38beba15193175270ee5479c409c5236817c4d
EmilianoAnaya <gael.anaya@alumnos.udg.mx>
2025-04-21 14:28:10 -0600

    Testing the nested structures

--- debugger.py
+++ debugger.py
@@ -4,7 +4,7 @@ import os
 
 def main():
     terminal = "hi"
-    file_name: str = "for_structure.f90"
+    file_name: str = "if_then_else.f90"
     compiler = Compiler(terminal, True)
     file_route = os.path.join(Routes.COMPILER_FILES.value, file_name)
     try:
